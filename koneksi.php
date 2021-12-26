5c91558db595b1d615832bf0b834e92b8d53e567
damskyyyy <93043524+damskyyyy@users.noreply.github.com>
2021-12-26 23:17:03 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php       #Melakukan koneksi dengan database akademik
+$host = "localhost";
+$username = "root";
+$password = "";
+$databasename = "akademik";
+$con = @mysqli_connect($host, $username, $password, $databasename);
+if (!$con) {
+    echo "Error: " . mysqli_connect_error();  
+    exit();         #Kondisi jika database gagal terhubung
+}
+?>
